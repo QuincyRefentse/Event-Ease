38eeb8160491b34a7534cb40259f4f32ea83e82b
QuincyRefentse <manwithheadache@gmail.com>
2025-04-25 12:46:15 +0000

    Latest changes

--- obj/Release/net8.0/EventEase.AssemblyInfo.cs
+++ obj/Release/net8.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e0ebacee83faa6745a2d27bb507375d91f9d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2597d308dec68bdec151d9f5bc14f447fbbe1f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
