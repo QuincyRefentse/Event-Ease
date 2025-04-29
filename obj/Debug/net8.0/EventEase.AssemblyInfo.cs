966dc6aee8bd18a9164bb2e9e881a3b73be03466
QuincyRefentse <manwithheadache@gmail.com>
2025-04-29 09:37:42 +0000

    Resolved merge conflicts

--- obj/Debug/net8.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2597d308dec68bdec151d9f5bc14f447fbbe1f68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38eeb8160491b34a7534cb40259f4f32ea83e82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
